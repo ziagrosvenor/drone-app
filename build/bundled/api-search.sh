19e9cf13fe7b0a054173bb91b94b02332ffa16ee
Zia Grosvenor <zia.grosvenor@momentumft.co.uk>
2016-07-09 23:20:56 +0100

    Update deps

--- build/bundled/api-search.sh
+++
@@ -1,4 +0,0 @@
-curl -v -X GET "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=cats&count=10&offset=0&mkt=en-us&safeSearch=Moderate"
--H "Ocp-Apim-Subscription-Key: {4fa9acc76e274c8f9f7f41e1d58bf367i}"
-
---data-ascii "{body}"
