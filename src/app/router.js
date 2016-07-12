880605a2143b7389ba9f7252250ee76d9dd258f3
ziagrosvenor <ziagrosvenor@live.co.uk>
2016-07-12 21:13:37 +0100

    Add change page / path action creator

--- src/app/router.js
+++ src/app/router.js
@@ -1,5 +1,7 @@
-import {fetchStrikes} from "./action-creators"
+import R from "ramda"
+import {fetchStrikes, changePage} from "./action-creators"
 
 export const routerFactory = ({dispatch}, pageSpec) => (path) => {
-  dispatch(fetchStrikes(pageSpec[path]))
+  dispatch(fetchStrikes(R.path(["data"], pageSpec[path])))
+  dispatch(changePage("/" + path))
 }
